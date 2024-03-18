79cc819f2b835a912e4e28e211c7ea956ec45e00
Coenraad Human <coen.human@gmail.com>
2024-03-18 15:20:36 +0200

    feat: immediately make rust available on terminal

--- setup.sh
+++ setup.sh
@@ -2,6 +2,10 @@
 
 set -euo pipefail
 
+# Install Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
+source "$HOME/.cargo/env"
+
+# Install cargo-update
 cargo install cargo-update
