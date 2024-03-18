6de6d137b458b476e99ca1c8d4c3d39c90a7e15c
Coenraad Human <coen.human@gmail.com>
2024-03-18 15:20:36 +0200

    fix: rust installation non-interactively

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@
 
 set -euo pipefail
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 cargo install cargo-update
